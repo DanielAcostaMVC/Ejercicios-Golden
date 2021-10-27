93a8517714866424d66c5a790fb415f1cd233cc6
Daniel Acosta <daniel.acosta@sinco.com.co>
2021-10-26 21:28:15 -0500

    final-coding bootcamp-modo ñoño

---
+++ Workouts/CodingBootcamp/Readme.md
@@ -0,0 +1,3 @@
+#Coding Bootcamp
+
+##En este ejercicio, se empieza a afianzar poco a poco los temas vistos de flex, position y responsive (quedo debiendo la animación); también se implementa la funcionalidad de slide mediante javascript, aplicando algunos conceptos aprendidos en capacitaciones de JS30
--- Workouts/CodingBootcamp/funciones.js
+++ Workouts/CodingBootcamp/funciones.js
@@ -1,30 +1,93 @@
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
-
 let currentPerson;
 let firstLetter;
 
-let profession; 
 
-prev.addEventListener("click", slide);
-next.addEventListener("click", slide);
+let profession = document.querySelector('#profession');
+let image = document.querySelector('.perfil').childNodes[0];
+let testimonial = document.querySelector('#testimony');
 
-function slide (){
+prev.addEventListener("click", () => {
+    slide(-1);
+});
+
+next.addEventListener("click", () => {
+    slide(1);
+});
+
+//DA: Tener en cuenta que para agregar más registros, el código debe corresponder con la primera letra del nombre.
+const datos =[{
+    "id": 1,
+    "codigo": "T",
+    "name": "Tanya Sinclair",
+    "profession": "UX Engineer",
+    "image": "assets/image-tanya.jpg",
+    "testimonial": '"I´ve been interested in coding for a while but never taken the jump, until now. I couldn´t recommend this course enough. I´m now in the job of my dreams and so excited about the future. "'
+  }, {
+    "id": 2,
+    "codigo": "J",
+    "name": "John Tarkpor",
+    "profession": "Junior Front-end Developer",
+    "image": "assets/image-john.jpg",
+    "testimonial": '" If you want to lay the best foundation possible I´d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. "'
+  }, {
+    "id": 3,
+    "codigo": "M",
+    "name": "Michelle",
+    "profession": "Decoration",
+    "image": "assets/avatar-michelle.jpg",
+    "testimonial": '" Me encanta la decoración, yo te recomiendo unos buenos muebles en tu hogar "'
+  }, {
+    "id": 4,
+    "codigo": "D",
+    "name": "Daniel Acosta",
+    "profession": "Golden",
+    "image": "assets/drasForestCamp.jpg",
+    "testimonial": '" Recomiendo el semillero de aprendiz y las plataformas como FreeCodeCamp y JS30...ya que me han ayudado a ir paso a paso, también gracias a la guía y las lecciones aprendidas de Didier. "'
+  }, {
+    "id": 5,
+    "codigo": "F",
+    "name": "Firulais",
+    "profession": "Dog-partner",
+    "image": "assets/lapiz.png",
+    "testimonial": '" Si quieres aprender a dibujar, primero debes estudiar la teoría de los colores. "'
+  }, {
+    "id": 6,
+    "codigo": "C",
+    "name": "Caballos",
+    "profession": "Coach mental suffering",
+    "image": "assets/horses.png",
+    "testimonial": '" Si me descuidas por mucho tiempo, no olvides que la montaña te recordará que debes tener mayor disciplina. "'
+  }];
+
+function slide (i){
+
+    let n ;
 
     currentPerson = document.querySelector('#person');
     firstLetter = currentPerson.innerHTML.substring(0,1);
-    profession = document.querySelector('#profession');
 
-    if (firstLetter==="T"){
-        currentPerson.innerHTML = "John Tarkpor";
-        profession.innerHTML = "Junior Front-end Developer";
+    const idActual = datos.filter(function(dato){
+        if(dato.codigo === firstLetter){
+            n = (dato.id-1) + i;
+        }
+    });
 
+    if (n>=datos.length){
+        n=0;
     }
-    else {
-        currentPerson.innerHTML = "Tanya Sinclair";
-        profession.innerHTML = "UX Engineer";
-    }
+
+    if (n<0){
+        n=datos.length-1;
+    } 
+    
+        currentPerson.innerHTML = datos[n].name;
+        profession.innerHTML = datos[n].profession;
+        image.src = datos[n].image;
+        testimonial.innerHTML = datos[n].testimonial;
+       
 };
 
-slide();
+slide(0); //predeterminado Tanya
