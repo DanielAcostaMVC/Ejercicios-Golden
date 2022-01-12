9e84c1eb6e15f691ab585e28c11a7b375ef7c271
Daniel Acosta <daniel.acosta@sinco.com.co>
2022-01-12 11:51:55 -0500

    Single Price - fin

---
+++ Workouts/SinglePriceGridComponent/funciones.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.button');
+
+button.addEventListener('click', guardar);
+
+function guardar(){
+    alert("Gracias por intentar matricularse, próximamente se habilitará el enlace!");
+}
